46ad1c45b46999886b9be7d835c6e5073a7c61d2
motyzk <motyzk@gmail.com>
2016-05-24 11:52:38 +0300

    added file for deleting specific collection

---
+++ delete_specific_collection.py
@@ -0,0 +1,16 @@
+import pymongo
+import sys
+
+collection_name = sys.argv[1]
+client = pymongo.MongoClient()
+db = client.get_database('socialagg')
+does_collection_exist = collection_name in db.collection_names()
+if does_collection_exist:
+    collection = db.get_collection(collection_name)
+    db.drop_collection(collection)
+    if collection_name in db.collection_names():
+        print("operation failed")
+    else:
+        print("collection deleted")
+else:
+    print("collection '{}' does not exist. didn't run operation".format(collection_name))
